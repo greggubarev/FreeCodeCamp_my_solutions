fd4387f8342302916dcd75d1934ee3a6036d94b7
Gregory Gubarev <g.gubarev63@gmail.com>
2018-12-02 23:54:50 +0400

    Create Truncate a String.js

---
+++ Basic Algorithm Scripting/Truncate a String.js
@@ -0,0 +1,21 @@
+/*
+Truncate a string (first argument) if it is longer than the given maximum string length (second argument). 
+Return the truncated string with a ... ending.
+*/
+
+// Added worked solution as a hint:
+//https://learn.freecodecamp.org/javascript-algorithms-and-data-structures/basic-algorithm-scripting/truncate-a-string
+//https://guide.freecodecamp.org/certifications/javascript-algorithms-and-data-structures/basic-algorithm-scripting/truncate-a-string/
+
+function truncateString(str, num) {
+  let temp = str.slice(0, num);
+  // return temp + "...";
+  if (str.length <= num) {
+    return str;
+  }
+  else {
+     return temp + "...";
+  }
+}
+
+truncateString("A-tisket a-tasket A green and yellow basket", 8);
