caa7cd6e72dd3f03945328fc2181d6d4828edc84
Gregory Gubarev <g.gubarev63@gmail.com>
2018-12-03 16:44:15 +0400

    Create Finders Keepers.js

---
+++ Basic Algorithm Scripting/Finders Keepers.js
@@ -0,0 +1,17 @@
+/*
+Create a function that looks through an array (first argument) and returns the first element in the array that passes a truth test (second argument). 
+If no element passes the test, return undefined.
+*/
+
+function findElement(arr, func) {
+  let num = 0;
+  for (let i = 0; i < arr.length; i++) {
+    num = arr[i];
+    if (func(num)) {
+      return num;
+    }
+  }
+  return undefined;
+}
+
+findElement([1, 2, 3, 4], num => num % 2 === 0);
