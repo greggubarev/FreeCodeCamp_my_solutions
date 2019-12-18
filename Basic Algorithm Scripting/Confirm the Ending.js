adf55ebb8be11da292006372e1f27402cf51fc68
Gregory Gubarev <g.gubarev63@gmail.com>
2019-12-18 18:39:21 +0400

    Update Confirm the Ending.js

--- Basic Algorithm Scripting/Confirm the Ending.js
+++ Basic Algorithm Scripting/Confirm the Ending.js
@@ -5,6 +5,20 @@ This challenge can be solved with the .endsWith() method, which was introduced i
 But for the purpose of this challenge, we would like you to use one of the JavaScript substring methods instead.
 */
 
+//First solution
+
+function confirmEnding(str, target) {
+   let newStr = str.substr(-target.length,target.length);
+   if (newStr == target) {
+     return true;
+   }
+    return false;
+}
+confirmEnding("Bastian", "b");
+
+
+//Second solution
+
 function confirmEnding(str, target) {
     return str.substring(str.length - target.length) === target;
 }
