1fca26071886a9cbca859ae928023726b7c95bc2
Gregory Gubarev <g.gubarev63@gmail.com>
2019-12-19 16:32:40 +0400

    Update Truncate a String.js

--- Basic Algorithm Scripting/Truncate a String.js
+++ Basic Algorithm Scripting/Truncate a String.js
@@ -25,5 +25,12 @@ function truncateString(str, num) {
   return (str.length <= num) ? str : str.slice(0, num) + "...";
 }
 
+// Third solution
+
+function truncateString(str, num) {
+
+  return (str.length > num) ? `${str.slice(0,num)}...` : str;
+
+}
 
 truncateString("A-tisket a-tasket A green and yellow basket", 8);
