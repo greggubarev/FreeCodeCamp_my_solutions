031f1c20de575ea4943eeb3ce7ee84c9ef937a8b
Gregory Gubarev <g.gubarev63@gmail.com>
2019-12-20 13:06:39 +0400

    Update Title Case a Sentence.js

--- Basic Algorithm Scripting/Title Case a Sentence.js
+++ Basic Algorithm Scripting/Title Case a Sentence.js
@@ -9,3 +9,13 @@ function titleCase(str) {
 }
 
 console.log(titleCase("HERE IS MY HANDLE HERE IS MY SPOUT"));
+
+
+// Jumba solution
+
+let str = "HERE IS MY HANDLE HERE IS MY SPOUT";
+    let arr = str.split(/\s/);
+    for (let i = 0; i < arr.length;i++){
+        let temp = arr[i].charAt(0).toUpperCase() + arr[i].substr(1).toLowerCase();
+        console.log(temp);
+    }
